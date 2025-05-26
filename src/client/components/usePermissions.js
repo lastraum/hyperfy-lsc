7ede4ff23c6d079de0cd68090c79f32a94bc04ae
Lastraum <lastraumonline@gmail.com>
2025-05-26 00:31:02 -0500

    updated sidebar to handle player role changes for building

--- src/client/components/usePermissions.js
+++ src/client/components/usePermissions.js
@@ -7,16 +7,23 @@ export function usePermissions(world) {
     const isBuilder = isAdmin || world.settings.public
     return { isAdmin, isBuilder }
   })
+
   useEffect(() => {
     function update() {
       const isAdmin = hasRole(world.entities.player?.data.roles, 'admin')
       const isBuilder = isAdmin || world.settings.public
       setPerms({ isAdmin, isBuilder })
     }
+
+    // Listen for both settings changes and player changes
     world.settings.on('change', update)
+    world.on('player', update)
+
     return () => {
       world.settings.off('change', update)
+      world.off('player', update)
     }
   }, [])
+
   return perms
 }
